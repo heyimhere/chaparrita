cef57337eb6fa19bf92003bc682af881b0b8e6e7
Silvers <logan.jma@gmail.com>
2024-11-03 02:55:31 -0600

    added gtags to the site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Chaparrita from './components/Chaparrita/Chaparrita';
+import GoogleAnalytics from './components/GoogleAnalytics/GoogleAnalytics';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+<GoogleAnalytics GA_MEASUREMENT_ID={process.env.REACT_APP_GA_MEASUREMENT_ID} />
 function App() {
+  console.log('process', process.env.REACT_APP_GA_MEASUREMENT_ID);
   return (
-    <>
-      <Chaparrita />
-    </> 
+    <Router>
+      <div>
+        <Routes>
+          <Route path='/' element={<Chaparrita />} />
+        </Routes>
+      </div>
+    </Router> 
    
   );
 }
---
+++ src/components/GoogleAnalytics/GoogleAnalytics.js
@@ -0,0 +1,37 @@
+import { useLocation, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import * as gtag from '../../lib/gtag';
+
+export default function GoogleAnalytics({GA_MEASUREMENT_ID}) {
+  const params = useParams();
+  const pathName = useLocation();
+  console.log('params', params);
+  console.log('pathName', pathName);
+
+  useEffect(() => {
+    const url = params + pathName.toString();
+
+    gtag.pageview(url);
+
+  }, [params, pathName]);
+
+  return (
+    <>
+      <script strategy="afterInteractive" 
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}/>
+      <script id='google-analytics' strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+        });
+        `,
+        }}
+      />
+    </>
+  )
+}
