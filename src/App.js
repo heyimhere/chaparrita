99aa0627b7cb52c0f1579e6f16dba20ec7c44658
Silvers <logan.jma@gmail.com>
2024-11-03 03:56:21 -0600

    initialized the google analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Chaparrita from './components/Chaparrita/Chaparrita';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import { Routes, Route } from 'react-router-dom';
 import usePageTracking from './lib/gtag';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_GA_MEASUREMENT_ID);
+  }, []);
+
   usePageTracking();
+
   return (
     <Routes>
       <Route path='/chaparrita' element={<Chaparrita />} />
